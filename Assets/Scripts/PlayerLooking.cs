762503fd4ee6342780765b278607836f92a86458
grolmabe <54779048+grolmabe@users.noreply.github.com>
2019-09-01 15:56:11 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerLooking.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLooking : MonoBehaviour
+{
+
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        //rotate the camera to the mouse's x or y value
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        //prevent the player from rotating too far up or down
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if(xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    //ensure's the limit on the players ability to rotate the camera upward is still limited.
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    private bool isJumping;
+    private Vector3 verticalMove;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        //get the movement input
+        float horizInput = Input.GetAxis(horizontalInputName);
+        float vertInput = Input.GetAxis(verticalInputName);
+
+        //apply movement
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        //charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        //get jump input
+        JumpInput();
+        if (!isJumping)
+        {
+            verticalMove = Physics.gravity;
+        }
+        charController.Move(((Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed) + verticalMove) * Time.deltaTime);
+    }
+
+    private void JumpInput()
+    {
+        //checks for the jump key being pressed
+        // The isGrounded check is to prevent jumping in midair after falling off something. (In that case, you are not jumping, but also are not grounded.) 
+        if (Input.GetKey(jumpKey) && !isJumping && charController.isGrounded)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    //controls jump
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            verticalMove = Vector3.up * jumpForce * jumpMultiplier;
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
+
+
