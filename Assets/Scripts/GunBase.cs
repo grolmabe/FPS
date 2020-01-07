8ada8abec506cb619c04d6ea54c1f51eafc408b6
ShadowHaxor99 <chrisfarmer28@gmail.com>
2020-01-07 10:01:40 -0500

    added something to make future level design easier, and some testing with the gun

---
+++ Assets/Scripts/GunBase.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour
+{
+    public float damage;
+    public float range;
+    public float fireRate;
+
+    public Camera PlayerCamera;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactFlash;
+    public GameObject impactDust;
+
+    public bool fullAuto;
+    private float nextTimeToFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fullAuto == true)
+        {
+            if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        } else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        //send out a raycast and print the name of the object it collided with
+        RaycastHit hit;
+        if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            GameObject impactFlashGameObject = Instantiate(impactFlash, hit.point, Quaternion.LookRotation(hit.normal));
+            GameObject impactDustGameObject = Instantiate(impactDust, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactFlashGameObject, 2.0f);
+            Destroy(impactDustGameObject, 2.0f);
+        }
+        //show the raycast in the scene window
+        Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.forward * range, Color.green, 5f);
+    }
+}
