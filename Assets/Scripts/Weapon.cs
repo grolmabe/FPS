f77a6a937736a1ba2e99d0ae64b7121512c77a0c
grolmabe <54779048+grolmabe@users.noreply.github.com>
2020-04-16 20:37:39 -0400

    Refactoring code, cleaning up, fixing a few things

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float activeHeight; // The height (y-coordinate) of the weapon when it is active, relative to PlayerCamera.
+    public float inactiveHeight; // The height (y-coordinate) of the weapon when it is inactive, relative to PlayerCamera.
+    public float damage; // The amount of damage this weapon does if it hits fully.
+    public float range; // The maximum range of this weapon. Objects further than range units away can't be hit.
+    public float fireRate = 0.0f; // The maximum rate, in shots per second, at which this weapon can fire. 
+    public ParticleSystem muzzleFlash;
+    public GameObject impactFlash;
+    public GameObject impactDust;
+    public bool fullAuto; // Is this a fully automatic weapon?
+    public enum WeaponType {raycast, projectile};
+    public WeaponType type;
+
+    private bool safetyOn = true; // Is the safety switch on. If so, the weapon can't be fired.
+    private float nextTimeToFire = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame if this object is active.
+    void Update()
+    {
+        // Check for the weapon being fired.
+        if (fullAuto == true)
+        {
+            // If this is an automatic weapon, request to fire if the fire button is currently being pressed.
+            if (Input.GetButton("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            // If this is a non-automatic weapon, request to fire if the fire button was pressed.
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+
+    }
+
+    public void Shoot()
+    {
+        if ((safetyOn == false) && ((fireRate == 0.0f) || (Time.time >= nextTimeToFire)))
+        {
+            muzzleFlash.Play();
+            if (type == WeaponType.raycast)
+            {
+                ShootRaycast();
+            }
+            else if (type == WeaponType.projectile)
+            {
+                ShootProjectile();
+            }
+            if (fireRate != 0.0f)
+            {
+                nextTimeToFire = Time.time + 1.0f / fireRate;
+            }
+        }
+    }
+
+    void ShootRaycast()
+    {
+        RaycastHit hit;
+
+        // Send out a raycast in the direction the weapon is aimed.
+        // We use the parent transform as the origin point, as that is the camera and gives a better experience.
+        if (Physics.Raycast(transform.parent.position, transform.parent.forward, out hit, range))
+        {
+            // The raycast hit an object.
+            // Debug.Log(hit.transform.name);
+            // Play the impact animations.
+            GameObject impactFlashGameObject = Instantiate(impactFlash, hit.point, Quaternion.LookRotation(hit.normal));
+            GameObject impactDustGameObject = Instantiate(impactDust, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactFlashGameObject, 2.0f);
+            Destroy(impactDustGameObject, 2.0f);
+            // Apply the damage to the object that was hit.
+        }
+        //show the raycast in the scene window
+        Debug.DrawRay(transform.parent.position, transform.parent.forward * range, Color.green, 5f);
+    }
+
+    void ShootProjectile()
+    {
+
+    }
+
+    public void SafetyOn()
+    {
+        safetyOn = true;
+    }
+
+    public void SafetyOff()
+    {
+        safetyOn = false;
+    }
+
+}
--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class WeaponSystem : MonoBehaviour
 {
     public GameObject[] weapons = null;
+    public float switchSpeed = 0.0f;
 
     private int weaponIndex = 0;
-    private GunBase currentWeapon = null;
-    private float nextTimeToFire;
+    private bool switchingWeapons = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,58 +19,93 @@ public class WeaponSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (currentWeapon != null)
+        // If we're in the process of switching to a new weapon, ignore any of these inputs.
+        if (switchingWeapons != true)
         {
-            if (currentWeapon.fullAuto == true)
+            // Check to see if the user has requested to switch weapons.
+            if (Input.GetButtonDown("Weapon1"))
             {
-                if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
-                {
-                    nextTimeToFire = Time.time + 1f / currentWeapon.fireRate;
-                    currentWeapon.Shoot();
-                }
+                SelectWeapon(0);
             }
-            else
+            else if (Input.GetButtonDown("Weapon2"))
             {
-                if (Input.GetButtonDown("Fire1"))
-                {
-                    currentWeapon.Shoot();
-                }
+                SelectWeapon(1);
+            }
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                SelectWeapon((weaponIndex + 1) % weapons.Length);
+            }
+            else if (Input.mouseScrollDelta.y < 0)
+            {
+                SelectWeapon(weaponIndex == 0 ? weapons.Length - 1 : weaponIndex - 1);
             }
         }
-        if (Input.GetButtonDown("Weapon1"))
-        {
-            SelectWeapon(0);
-        }
-        else if (Input.GetButtonDown("Weapon2"))
-        {
-            SelectWeapon(1);
-        }
-        if (Input.mouseScrollDelta.y > 0)
-        {
-            SelectWeapon((weaponIndex + 1) % weapons.Length);
-        }
-        else if (Input.mouseScrollDelta.y < 0)
-        {
-            SelectWeapon(weaponIndex == 0 ? weapons.Length - 1 : weaponIndex - 1);
-        }
-
     }
 
     void SelectWeapon(int index)
     {
-        //Debug.Log("Asked to select weapon " + index);
-        if ((weapons != null) && (index >=0) && (index < weapons.Length))
+        //Debug.Log("Switching from weapon index " + weaponIndex + " to weapon index " + index);
+        StartCoroutine(SwitchWeapon(index));
+    }
+
+    IEnumerator SwitchWeapon(int index)
+    {
+        float newY;
+        Weapon newWeapon;
+        Weapon currentWeapon;
+
+        // Switch from the current weapon to the weapon specified by the given index, including animating it.
+        //Debug.Log("In SwitchWeapon().");
+        // Check to make sure we have weapons and that the requested weapon exists.
+        if ((weapons != null) && (index >= 0) && (index < weapons.Length) && (weapons[index] != null))
         {
-            if (weapons[weaponIndex] != null)
+            newWeapon = weapons[index].GetComponent<Weapon>();
+            // Only need to switch weapons if the requested weapon isn't the currently selected one.
+            if (index != weaponIndex)
             {
-                weapons[weaponIndex].SetActive(false);
+                switchingWeapons = true;
+                // Lower the current weapon.
+                if (weapons[weaponIndex] != null)
+                {
+                    //Debug.Log("Lowering current weapon.");
+                    currentWeapon = weapons[weaponIndex].GetComponent<Weapon>();
+                    if (currentWeapon != null)
+                    {
+                        currentWeapon.SafetyOn();
+                        while (weapons[weaponIndex].transform.localPosition.y > currentWeapon.inactiveHeight)
+                        {
+                            newY = weapons[weaponIndex].transform.localPosition.y - switchSpeed * Time.deltaTime;
+                            weapons[weaponIndex].transform.localPosition = new Vector3(weapons[weaponIndex].transform.localPosition.x, newY, weapons[weaponIndex].transform.localPosition.z);
+                            //Debug.Log("Height: " + weapons[weaponIndex].transform.localPosition.y + "  Target: " + currentWeapon.inactiveHeight);
+                            yield return null;
+                        }
+                    }
+                    weapons[weaponIndex].SetActive(false);
+                }
+                // Raise the new weapon.
+                //Debug.Log("Raising new weapon.");
+                weapons[index].SetActive(true);
+                if (newWeapon != null)
+                {
+                    while (weapons[index].transform.localPosition.y < newWeapon.activeHeight)
+                    {
+                        newY = weapons[index].transform.localPosition.y + switchSpeed * Time.deltaTime;
+                        weapons[index].transform.localPosition = new Vector3(weapons[index].transform.localPosition.x, newY, weapons[index].transform.localPosition.z);
+                        //Debug.Log("Height: " + weapons[index].transform.localPosition.y + "  Target: " + newWeapon.activeHeight);
+                        yield return null;
+                    }
+                }
+                switchingWeapons = false;
             }
-            if (weapons[index] != null)
+            // Turn the safety off on the selected weapon.
+            if (newWeapon != null)
             {
-                weaponIndex = index;
-                currentWeapon = weapons[index].GetComponent<GunBase>();
-                weapons[weaponIndex].SetActive(true);
+                newWeapon.SafetyOff();
             }
+            // Update the index of the currently selected weapon.
+            weaponIndex = index;
         }
+        //Debug.Log("Leaving SwitchWeapon().");
+        yield return null;
     }
 }
