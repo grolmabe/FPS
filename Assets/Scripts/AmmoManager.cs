72937f50ca92578fc8b66b08767995f1d2b3a927
grolmabe <54779048+grolmabe@users.noreply.github.com>
2020-04-16 22:03:07 -0400

    Added ammo manager and modified weapons to use it.
    The AmmoManager class implements an inventory of different types of ammo, with the ability to remove or add inventory.
    Modified the Weapon class to have an associated ammo manager and to remove ammo as it needs it, no longer firing if ammo has run out.

---
+++ Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour
+{
+    public enum AmmoType { bullets, plasmaUnits, shotgunShells }
+    public int[] count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Remove(AmmoType type, int numRequested, bool allOrNone = true)
+    {
+        int numReturned = 0;
+
+        if ( numRequested > count[(int)type])
+        {
+            if (allOrNone == false)
+            {
+                numReturned = count[(int)type];
+                count[(int)type] = 0;
+            }
+        }
+        else if ( numRequested > 0 )
+        {
+            numReturned = numRequested;
+            count[(int)type] -= numRequested;
+        }
+        return numReturned;
+    }
+
+    public int Add(AmmoType type, int num)
+    {
+        if (num > 0)
+        {
+            count[(int)type] += num;
+        }
+        return count[(int)type];
+    }
+
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,12 +9,17 @@ public class Weapon : MonoBehaviour
     public float damage; // The amount of damage this weapon does if it hits fully.
     public float range; // The maximum range of this weapon. Objects further than range units away can't be hit.
     public float fireRate = 0.0f; // The maximum rate, in shots per second, at which this weapon can fire. 
-    public ParticleSystem muzzleFlash;
-    public GameObject impactFlash;
-    public GameObject impactDust;
+    public ParticleSystem muzzleFlash; // The ParticleSystem that implements the muzzle flash for this weapon.
+    public GameObject impactFlash; // The object that implements the impact flash effect for this weapon.
+    public GameObject impactDust; // The object that implements the impact dust effect for this weapon.
+    public float impactFlashLifespan = 1.0f;
+    public float impactDustLifespan = 1.0f;
     public bool fullAuto; // Is this a fully automatic weapon?
     public enum WeaponType {raycast, projectile};
-    public WeaponType type;
+    public WeaponType type; // Is this a raycast weapon, or does it shoot projectiles that are actual objects in the scene?
+    public AmmoManager ammo;
+    public int roundsPerShot;
+    public AmmoManager.AmmoType ammoType;
 
     private bool safetyOn = true; // Is the safety switch on. If so, the weapon can't be fired.
     private float nextTimeToFire = 0.0f;
@@ -51,7 +56,7 @@ public class Weapon : MonoBehaviour
 
     public void Shoot()
     {
-        if ((safetyOn == false) && ((fireRate == 0.0f) || (Time.time >= nextTimeToFire)))
+        if ((safetyOn == false) && ((fireRate == 0.0f) || (Time.time >= nextTimeToFire)) && (ammo.Remove(ammoType, roundsPerShot) == roundsPerShot))
         {
             muzzleFlash.Play();
             if (type == WeaponType.raycast)
@@ -79,15 +84,15 @@ public class Weapon : MonoBehaviour
         {
             // The raycast hit an object.
             // Debug.Log(hit.transform.name);
-            // Play the impact animations.
+            // Create the impact effects at the point of impact.
             GameObject impactFlashGameObject = Instantiate(impactFlash, hit.point, Quaternion.LookRotation(hit.normal));
             GameObject impactDustGameObject = Instantiate(impactDust, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactFlashGameObject, 2.0f);
-            Destroy(impactDustGameObject, 2.0f);
+            Destroy(impactFlashGameObject, impactFlashLifespan);
+            Destroy(impactDustGameObject, impactDustLifespan);
             // Apply the damage to the object that was hit.
         }
         //show the raycast in the scene window
-        Debug.DrawRay(transform.parent.position, transform.parent.forward * range, Color.green, 5f);
+        //Debug.DrawRay(transform.parent.position, transform.parent.forward * range, Color.green, 5f);
     }
 
     void ShootProjectile()
